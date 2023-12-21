792b15fb443998fb827dd9435548be12d54c7d83
AdiXaK <113100045+AdiXaK@users.noreply.github.com>
2023-12-22 00:19:40 +0300

    лифт готов

---
+++ Assets/scripts and settings/raycast/script/ObjectInteraction.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public float interactionRange = 2f;
+    public bool IsButtonActive = false;
+    public Animator animator;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, interactionRange))
+            {
+                if (hit.collider.CompareTag("Panel"))
+                {
+                    InteractWithObject(hit.collider.gameObject);
+                }
+                if (hit.collider.CompareTag("Button") && IsButtonActive) 
+                {
+                    animator.SetBool("IsOpen", true);
+                }
+                if (hit.collider.CompareTag("Butto2") && IsButtonActive)
+                {
+                    animator.SetBool("IsOpen", false);
+                    animator.SetBool("Up", true);
+
+                }
+            }
+        }
+    }
+
+    void InteractWithObject(GameObject obj)
+    {
+        Debug.Log("Interacted with: " + obj.name);
+        IsButtonActive = true;
+    }
+}
